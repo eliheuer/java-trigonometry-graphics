842d23d8e6a246d36ddb716e28bcf3b8b259c6d4
Eli Heuer <eliheuer@gmail.com>
2017-11-28 01:42:13 -0500

    Graphics update

--- src/BasicTrig.java
+++ src/BasicTrig.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
@@ -14,7 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class CircleIntersectionTest {
+public class BasicTrig {
  
     public static void main(String[] args) throws IOException {
         SwingUtilities.invokeLater(new Runnable() {
@@ -31,17 +32,17 @@ public class CircleIntersectionTest {
         int frameH = 768;
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new CircleIntersectionPanel());
+        frame.getContentPane().add(new BasicTrigPanel());
         frame.setSize(frameW, frameH);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
 
-class CircleIntersectionPanel extends JPanel implements MouseMotionListener {
+class BasicTrigPanel extends JPanel implements MouseMotionListener {
     private Point mousePosition = null;
 
-    CircleIntersectionPanel() {
+    BasicTrigPanel() {
         addMouseMotionListener(this);
     }
 
@@ -49,70 +50,90 @@ class CircleIntersectionPanel extends JPanel implements MouseMotionListener {
     protected void paintComponent(Graphics gr) {
         super.paintComponent(gr);
         Graphics2D g = (Graphics2D)gr;
-        
+	g.setRenderingHint(
+            RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
+
+	// Set colors
+	Color bgColor = Color.decode("#282A36");
+	Color fgColor = Color.decode("#D8D8D8");
+	Color mdColor = Color.decode("#555555");
+	Color reColor = Color.decode("#F02828");
+	Color orColor = Color.decode("#FFA000");
+	Color yeColor = Color.decode("#F4D03F");
+	Color grColor = Color.decode("#27AE60");
+	Color blColor = Color.decode("#3498DB");
+	Color puColor = Color.decode("#9B59B6");
+	
         // Set font
         int frameW = 768;
         int frameH = 768;
-        int fontSize = frameH/32; 
-        Font font = new Font("freeMono", Font.BOLD, fontSize);
+        int fontSize = frameH/16; 
+        Font font = new Font("InputMonoCompressed", Font.BOLD, fontSize);
         g.setFont(font);
-       
+
+	// Basic variables
         double centerX = getWidth() / 2;
         double centerY = getHeight() / 2;
-        double radius = 128;
+        double radius = 256;
 
-        
+	// Set BG
+        g.setColor(bgColor);;
+	g.fillRect(0, 0, 2048, 2048);
 
-        g.setStroke(new BasicStroke(6));
-        g.setColor(Color.BLACK);;
+	// Grid
+	g.setColor(mdColor);
+	g.setStroke(new BasicStroke(2));
+        for (int i=0;i<=512;i+=64) {
+	    g.draw(new Line2D.Double((centerX-radius)+i, 0,
+				     (centerX-radius)+i, 2048));
+	}
+	for (int j=0;j<=512;j+=64) {
+	    g.draw(new Line2D.Double(0, (centerY-radius)+j,
+				     2048, (centerY-radius)+j));
+	}
+	
+	// Draw circle
+	g.setStroke(new BasicStroke(6));
+        g.setColor(fgColor);
         g.draw(new Ellipse2D.Double(
             centerX-radius, centerY-radius, 
             radius+radius, radius+radius));
-        if (mousePosition == null)
-        {
-            return;
-        }
+        if (mousePosition == null) {return;}
 
-
-        g.setColor(Color.BLUE);
-
+	// Get mouse input variables
         double x = (mousePosition.x - centerX) / radius;
         double y = computeY(x);
-
         double cx = centerX + radius * x;
         double cy = centerY - radius * y;
-        g.fill(new Ellipse2D.Double(cx-8, cy-8, 16, 16));
+	
+        // Draw triangle 
+        g.setColor(reColor);
+        g.draw(new Line2D.Double(mousePosition.x, centerY, mousePosition.x, cy));
+        g.draw(new Line2D.Double(centerX, centerY, cx, cy)); 
+        g.draw(new Line2D.Double(centerX, centerY, mousePosition.x, centerY));
 
-        //new 
-        g.setColor(Color.RED);
-        g.draw(new Line2D.Double(
-            mousePosition.x, centerY, mousePosition.x, cy));
-        g.draw(new Line2D.Double(
-            centerX, centerY, cx, cy)); 
-        g.draw(new Line2D.Double(
-            // centerX-radius, centerY, 
-            // centerX+radius, centerY));
-            centerX, centerY,
-            mousePosition.x, centerY));
+	// Blue points // BLUE COLOR
+        g.setColor(blColor);
+        g.fill(new Ellipse2D.Double(cx-16, cy-16, 32, 32));
+	g.drawString("A", (int)cx+16, (int)cy-16);
+	g.setColor(yeColor);
+	g.fill(new Ellipse2D.Double(mousePosition.x-16, centerY-16, 32, 32));
+	g.drawString("B", mousePosition.x+16, (int)centerY-16);
+	g.setColor(grColor);
+	g.fill(new Ellipse2D.Double(centerX-16, centerY-16, 32, 32));
+	g.drawString("C", (int)centerX+16, (int)centerY-16);
 
-        
-        g.setColor(Color.BLUE);
-        g.fill(new Ellipse2D.Double(cx-8, cy-8, 24, 24));
-        g.fill(new Ellipse2D.Double(centerX-8, centerY-8, 24, 24));
-        g.fill(new Ellipse2D.Double(mousePosition.x-8, centerY-8, 16, 16));
+	// Screen Text // BLACK COLOR
+        g.setColor(fgColor);
+        // g.drawString("x pos: "+x, 16, 32);
+        // g.drawString("y pos: "+y, 16, 64);
+        g.drawString("Angle θ: "+Math.toDegrees(Math.acos(x)), mousePosition.x, 64);
 
-
-
-        // 32,64,96,128,160,192,224,256,288,320,352,384,416,448,
-        g.setColor(Color.BLACK);
-        g.drawString("x pos: "+x, 16, 32);
-        g.drawString("y pos: "+y, 16, 64);
-        g.drawString("cx - : "+cx, 16, 96);
-        g.drawString("cy - : "+cy, 16, 128);
-        g.drawString("angle: "+Math.toDegrees(Math.acos(x)), 16, 160);
-
+	
+	
     }
-
+    
     private static double computeY(double x)
     {
         return Math.sin(Math.acos(x));
