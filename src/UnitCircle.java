49afe834d294ff2a8ba4bb996513289664da3fe7
Eli Heuer <eliheuer@gmail.com>
2017-12-07 10:57:17 -0500

    Adding unit circle example

--- src/UnitCircle.java
+++ src/UnitCircle.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
+import java.text.NumberFormat;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
@@ -15,8 +16,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class BasicTrig {
- 
+public class UnitCircle {
+
     public static void main(String[] args) throws IOException {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -28,21 +29,21 @@ public class BasicTrig {
 
     private static void createAndShowGUI() {
         // TODO make final
-        int frameW = 768;
+        int frameW = 1024;
         int frameH = 768;
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(new BasicTrigPanel());
+        frame.getContentPane().add(new UnitCirclePanel());
         frame.setSize(frameW, frameH);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 }
 
-class BasicTrigPanel extends JPanel implements MouseMotionListener {
+class UnitCirclePanel extends JPanel implements MouseMotionListener {
     private Point mousePosition = null;
 
-    BasicTrigPanel() {
+    UnitCirclePanel() {
         addMouseMotionListener(this);
     }
 
@@ -52,7 +53,7 @@ class BasicTrigPanel extends JPanel implements MouseMotionListener {
         Graphics2D g = (Graphics2D)gr;
 	g.setRenderingHint(
             RenderingHints.KEY_ANTIALIASING,
-             RenderingHints.VALUE_ANTIALIAS_ON);
+            RenderingHints.VALUE_ANTIALIAS_ON);
 
 	// Set colors
 	Color bgColor = Color.decode("#282A36");
@@ -68,8 +69,10 @@ class BasicTrigPanel extends JPanel implements MouseMotionListener {
         // Set font
         int frameW = 768;
         int frameH = 768;
-        int fontSize = frameH/16; 
-        Font font = new Font("InputMonoCompressed", Font.BOLD, fontSize);
+        int fontSizeSM = 20;
+	int fontSizeMD = 26;
+	int fontSizeLG = 36;
+        Font font = new Font("InputMonoCompressed", Font.BOLD, fontSizeSM);
         g.setFont(font);
 
 	// Basic variables
@@ -94,7 +97,7 @@ class BasicTrigPanel extends JPanel implements MouseMotionListener {
 	}
 	
 	// Draw circle
-	g.setStroke(new BasicStroke(6));
+	g.setStroke(new BasicStroke(4));
         g.setColor(fgColor);
         g.draw(new Ellipse2D.Double(
             centerX-radius, centerY-radius, 
@@ -103,32 +106,65 @@ class BasicTrigPanel extends JPanel implements MouseMotionListener {
 
 	// Get mouse input variables
         double x = (mousePosition.x - centerX) / radius;
+	System.out.println("\n(mPos.x("+mousePosition.x+")-centerX("+centerX+"))= "+(mousePosition.x - centerX));
+	System.out.println("x=(mPos.x("+mousePosition.x+")-centerX("+centerX+"))/radius("+radius+")");
         double y = computeY(x);
         double cx = centerX + radius * x;
         double cy = centerY - radius * y;
 	
         // Draw triangle 
         g.setColor(reColor);
-        g.draw(new Line2D.Double(mousePosition.x, centerY, mousePosition.x, cy));
-        g.draw(new Line2D.Double(centerX, centerY, cx, cy)); 
-        g.draw(new Line2D.Double(centerX, centerY, mousePosition.x, centerY));
+        //g.draw(new Line2D.Double(mousePosition.x, centerY, mousePosition.x, cy));
 
-	// Blue points // BLUE COLOR
-        g.setColor(blColor);
-        g.fill(new Ellipse2D.Double(cx-16, cy-16, 32, 32));
-	g.drawString("A", (int)cx+16, (int)cy-16);
-	g.setColor(yeColor);
-	g.fill(new Ellipse2D.Double(mousePosition.x-16, centerY-16, 32, 32));
-	g.drawString("B", mousePosition.x+16, (int)centerY-16);
-	g.setColor(grColor);
-	g.fill(new Ellipse2D.Double(centerX-16, centerY-16, 32, 32));
-	g.drawString("C", (int)centerX+16, (int)centerY-16);
-	// C and B midpoint
+ 
+
+
+	// Draw Sum and Difference 
+        //g.setColor(puColor);
+        //g.draw(new Line2D.Double(mousePosition.x, centerY, mousePosition.x, cy));
+        //g.draw(new Line2D.Double(centerX, centerY, cx, cy)); 
+	// g.draw(new Line2D.Double(centerX, centerY, mousePosition.x, centerY));
+
+	// MAin tri points...
+	Point pA = new Point((int)cx, (int)cy);
+	Point pB = new Point((int)mousePosition.x, (int)centerY);
+	Point pC = new Point((int)centerX, (int)centerY);
+
+        // NumberFormat formatter = new DecimalFormat("#0.00");     
+        // System.out.println();
+	
+	// A point
 	g.setColor(orColor);
-	g.fill(new Ellipse2D.Double((((cx-16)+(centerX-16))/2),
-				    (((cy-16)+(centerY-16))/2), 32, 32));
-	g.drawString("midpoint", (int)(((cx-16)+(centerX-16))/2),
-		                 (int)(((cy-16)+(centerY-16))/2));
+	g.draw(new Line2D.Double(pA.x, pA.y, pB.x, pB.y));
+
+	// B point--Yellow 
+	g.setColor(grColor);
+	g.draw(new Line2D.Double(pB.x, pB.y, pC.x, pC.y));
+ 
+	g.setColor(yeColor);
+	g.fill(new Ellipse2D.Double(pB.x-10, pB.y-10, 20, 20));
+	g.drawString("B("+pB.x+", "+pB.y+")", (int)pB.x+10, (int)pB.y-30);
+
+	// C point--Green
+	g.setColor(puColor);
+	g.draw(new Line2D.Double(pC.x, pC.y, pA.x, pA.y));
+
+	g.setColor(blColor);
+	g.fill(new Ellipse2D.Double(pC.x-10, pC.y-10, 20, 20));
+	g.drawString("C("+pC.x+", "+pC.y+")", (int)pC.x+10, (int)pC.y-30);
+
+	// point A
+	g.setColor(reColor);
+        g.fill(new Ellipse2D.Double(pA.x-10, pA.y-10, 20, 20));
+	g.drawString("A("+x+", "+y+")", (int)pA.x+10, (int)pA.y-30);
+	
+	// Hyp, C and B midpoint
+	g.setColor(reColor);
+	//g.fill(new Ellipse2D.Double((((cx-10)+(centerX-10))/2),
+	//			    (((cy-10)+(centerY-10))/2), 10, 10));
+	
+	g.drawString("Hyp", (int)(((cx-10)+(centerX-10))/2),
+		                 (int)(((cy-10)+(centerY-10))/2));
 
 	// Screen Text // BLACK COLOR
         Font fontText = new Font("InputMonoCompressed", Font.BOLD, 24);
@@ -138,9 +174,15 @@ class BasicTrigPanel extends JPanel implements MouseMotionListener {
         // g.drawString("x pos: "+x, 16, 32);
         // g.drawString("y pos: "+y, 16, 64);
         g.drawString("Angle θ: "+Math.toDegrees(Math.acos(x)), 76, 32);	
-    }
+     }
     
     private static double computeY(double x) {
+	System.out.println("x in computeY="+x);
+	double x2 = Math.sin(Math.acos(x)); 
+	System.out.println("Math.sin(Math.acos(x))="+x2);
+	double x3 = Math.acos(x);
+	System.out.println("Math.acos(x))="+x3);
+	// g.drawString("A: "+Math.toDegrees(Math.acos(x)), 76, 96);	
         return Math.sin(Math.acos(x));
     }
 
