ce2f41da58f53a84771db45f3d973c7e729f2750
Eli Heuer <eliheuer@gmail.com>
2017-12-07 19:23:09 -0500

    Updating unit circle example

--- src/UnitCircle.java
+++ src/UnitCircle.java
@@ -51,57 +51,115 @@ class UnitCirclePanel extends JPanel implements MouseMotionListener {
     protected void paintComponent(Graphics gr) {
         super.paintComponent(gr);
         Graphics2D g = (Graphics2D)gr;
-	g.setRenderingHint(
+        g.setRenderingHint(
             RenderingHints.KEY_ANTIALIASING,
             RenderingHints.VALUE_ANTIALIAS_ON);
 
-	// Set colors
-	Color bgColor = Color.decode("#282A36");
-	Color fgColor = Color.decode("#D8D8D8");
-	Color mdColor = Color.decode("#555555");
-	Color reColor = Color.decode("#F02828");
-	Color orColor = Color.decode("#FFA000");
-	Color yeColor = Color.decode("#F4D03F");
-	Color grColor = Color.decode("#27AE60");
-	Color blColor = Color.decode("#3498DB");
-	Color puColor = Color.decode("#9B59B6");
-	
+        // Set colors
+        Color bgColor = Color.decode("#282A36");
+        Color fgColor = Color.decode("#D8D8D8");
+        Color mdColor = Color.decode("#555555");
+        Color reColor = Color.decode("#F02828");
+        Color orColor = Color.decode("#FFA000");
+        Color yeColor = Color.decode("#F4D03F");
+        Color grColor = Color.decode("#27AE60");
+        Color blColor = Color.decode("#3498DB");
+        Color puColor = Color.decode("#9B59B6");
+
         // Set font
         int frameW = 768;
         int frameH = 768;
         int fontSizeSM = 20;
-	int fontSizeMD = 26;
-	int fontSizeLG = 36;
+        int fontSizeMD = 26;
+        int fontSizeLG = 36;
         Font font = new Font("InputMonoCompressed", Font.BOLD, fontSizeSM);
         g.setFont(font);
 
-	// Basic variables
+        // Basic variables
         double centerX = getWidth() / 2;
         double centerY = getHeight() / 2;
         double radius = 256;
 
-	// Set BG
+        // Set BG
         g.setColor(bgColor);;
-	g.fillRect(0, 0, 2048, 2048);
+        g.fillRect(0, 0, 2048, 2048);
 
-	// Grid
-	g.setColor(mdColor);
-	g.setStroke(new BasicStroke(2));
-        for (int i=0;i<=512;i+=64) {
-	    g.draw(new Line2D.Double((centerX-radius)+i, 0,
-				     (centerX-radius)+i, 2048));
-	}
+        // Grid
+        g.setColor(mdColor);
+        g.setStroke(new BasicStroke(2));
+
+	// Y
+	for (int i=0;i<=512;i+=64) {
+            g.draw(new Line2D.Double((centerX-radius)+i, (centerY-radius),
+				     (centerX-radius)+i, (centerY+radius)));
+        }
+
+	// X
 	for (int j=0;j<=512;j+=64) {
-	    g.draw(new Line2D.Double(0, (centerY-radius)+j,
-				     2048, (centerY-radius)+j));
-	}
-	
-	// Draw circle
-	g.setStroke(new BasicStroke(4));
+            g.draw(new Line2D.Double((centerX-radius), (centerY-radius)+j,
+                                     (centerX+radius), (centerY-radius)+j));
+        }
+
+	// Mid	// X
         g.setColor(fgColor);
+
+	g.draw(new Line2D.Double((centerX-(radius+64)),  (centerY),
+				 (centerX+(radius+64)), (centerY)));
+
+	// (-1, 0)
+	g.draw(new Line2D.Double((centerX-(radius+64)),  (centerY),
+				 (centerX-(radius+56)), (centerY+8)));
+	g.draw(new Line2D.Double((centerX-(radius+64)),  (centerY),
+				 (centerX-(radius+56)), (centerY-8)));
+
+	// (1, 0)
+	g.draw(new Line2D.Double((centerX+(radius+64)),  (centerY),
+				 (centerX+(radius+56)), (centerY+8)));
+	g.draw(new Line2D.Double((centerX+(radius+64)),  (centerY),
+				 (centerX+(radius+56)), (centerY-8)));
+
+	// y
+	g.draw(new Line2D.Double((centerX), (centerY-(radius+64)),
+				 (centerX), (centerY+(radius+64))));
+
+	// (0, -1)
+	g.draw(new Line2D.Double((centerX), (centerY-(radius+64)),
+				 (centerX+8), (centerY-(radius+56))));
+	g.draw(new Line2D.Double((centerX), (centerY-(radius+64)),
+				 (centerX-8), (centerY-(radius+56))));
+
+	// (0, 1)
+	g.draw(new Line2D.Double((centerX), (centerY+(radius+64)),
+				 (centerX+8), (centerY+(radius+56))));
+	g.draw(new Line2D.Double((centerX), (centerY+(radius+64)),
+				 (centerX-8), (centerY+(radius+56))));
+
+	// main points
+	g.fill(new Ellipse2D.Double((centerX+radius)-10, centerY-10, 20, 20));
+	g.drawString("(1,0)", (int)(centerX+radius)+10, (int)centerY-20);
+
+	g.fill(new Ellipse2D.Double((centerX-radius)-10, centerY-10, 20, 20));
+	g.drawString("(-1,0)", (int)(centerX-radius)-72, (int)centerY-20);
+
+	g.fill(new Ellipse2D.Double(centerX-10, (centerY+radius)-10, 20, 20));
+	g.drawString("(0,-1)", (int)centerX+10, (int)(centerY+radius)+30);
+
+	g.fill(new Ellipse2D.Double(centerX-10, (centerY-radius)-10, 20, 20));
+	g.drawString("(0,1)", (int)centerX+10, (int)(centerY-radius)-30);
+	
+        // Draw circle
+	g.setColor(puColor);
+        g.draw(new Ellipse2D.Double(centerX-(radius/2), centerY-(radius/2),
+				    radius, radius));
+	
+        g.setStroke(new BasicStroke(4));
+
+	g.setColor(fgColor);
         g.draw(new Ellipse2D.Double(
-            centerX-radius, centerY-radius, 
+            centerX-radius, centerY-radius,
             radius+radius, radius+radius));
+        g.setColor(fgColor);
+
         if (mousePosition == null) {return;}
 
 	// Get mouse input variables
@@ -130,50 +188,77 @@ class UnitCirclePanel extends JPanel implements MouseMotionListener {
 	Point pB = new Point((int)mousePosition.x, (int)centerY);
 	Point pC = new Point((int)centerX, (int)centerY);
 
-        // NumberFormat formatter = new DecimalFormat("#0.00");     
+        // NumberFormat fmtr = new DecimalFormat("#0.00");     
         // System.out.println();
 	
 	// A point
-	g.setColor(orColor);
+	g.setColor(reColor);
 	g.draw(new Line2D.Double(pA.x, pA.y, pB.x, pB.y));
 
 	// B point--Yellow 
-	g.setColor(grColor);
+	g.setColor(reColor);
 	g.draw(new Line2D.Double(pB.x, pB.y, pC.x, pC.y));
  
 	g.setColor(yeColor);
 	g.fill(new Ellipse2D.Double(pB.x-10, pB.y-10, 20, 20));
-	g.drawString("B("+pB.x+", "+pB.y+")", (int)pB.x+10, (int)pB.y-30);
+	g.drawString("B [x=cos(θ)="+String.format( "%.2f",x)+
+                       ", 0]", (int)pB.x+10, (int)pB.y+30);
 
 	// C point--Green
-	g.setColor(puColor);
+	g.setColor(reColor);
 	g.draw(new Line2D.Double(pC.x, pC.y, pA.x, pA.y));
 
-	g.setColor(blColor);
+	g.setColor(grColor);
 	g.fill(new Ellipse2D.Double(pC.x-10, pC.y-10, 20, 20));
-	g.drawString("C("+pC.x+", "+pC.y+")", (int)pC.x+10, (int)pC.y-30);
+	g.drawString("C [0, 0]", (int)pC.x+10, (int)pC.y+30);
 
 	// point A
-	g.setColor(reColor);
+	g.setColor(blColor);
         g.fill(new Ellipse2D.Double(pA.x-10, pA.y-10, 20, 20));
-	g.drawString("A("+x+", "+y+")", (int)pA.x+10, (int)pA.y-30);
-	
-	// Hyp, C and B midpoint
-	g.setColor(reColor);
-	//g.fill(new Ellipse2D.Double((((cx-10)+(centerX-10))/2),
-	//			    (((cy-10)+(centerY-10))/2), 10, 10));
-	
-	g.drawString("Hyp", (int)(((cx-10)+(centerX-10))/2),
-		                 (int)(((cy-10)+(centerY-10))/2));
+	g.drawString("A [x=cos(θ)="+String.format( "%.2f",x)+
+		      ", y=sin(θ) "+String.format( "%.2f",y)+"]", (int)pA.x+10, (int)pA.y-30);
 
+
+
+       
+	
+	// Adj, C and B midpoint
+	g.setColor(orColor);
+	g.fill(new Ellipse2D.Double((((pB.x-5)+(pC.x-5))/2),
+				    (((pB.y-5)+(pC.y-5))/2), 10, 10));
+	
+	g.drawString("Adj", (int)(((pB.x-30)+(pC.x-0))/2),
+		                 (int)(((pB.y-10)+(pC.y-10))/2));
+
+	
+	// Hyp, C and A midpoint
+	g.fill(new Ellipse2D.Double((((cx-5)+(centerX-5))/2),
+				    (((cy-5)+(centerY-5))/2), 10, 10));
+	
+	g.drawString("Hyp", (int)(((cx-30)+(centerX))/2),
+		                 (int)(((cy-30)+(centerY))/2));
+
+	// Opp, B and A midpoint
+	g.fill(new Ellipse2D.Double((((pB.x-5)+(pA.x-5))/2),
+				    (((pB.y-5)+(pA.y-5))/2), 10, 10));
+	
+	g.drawString("Opp", (int)(((pB.x)+(pA.x+20))/2),
+		                 (int)(((pB.y+15)+(pA.y))/2));
+
+
+
+
+	
 	// Screen Text // BLACK COLOR
         Font fontText = new Font("InputMonoCompressed", Font.BOLD, 24);
         g.setFont(fontText);
 
-        g.setColor(fgColor);
+	//        g.setColor(fgColor);
         // g.drawString("x pos: "+x, 16, 32);
         // g.drawString("y pos: "+y, 16, 64);
-        g.drawString("Angle θ: "+Math.toDegrees(Math.acos(x)), 76, 32);	
+        g.setColor(puColor);
+	g.drawString("Angle θ: "+String.format( "%.2f",Math.toDegrees(Math.acos(x))),
+		     (int)(centerX-radius), (int)(centerY-(radius+10)));	
      }
     
     private static double computeY(double x) {
